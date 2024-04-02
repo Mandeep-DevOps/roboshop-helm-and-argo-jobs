382e1b70426ba8924cf00b07e36698dbdb71966f
SVR Kalluru <raghuk.vit@gmail.com>
2024-04-02 08:33:19 +0530

    Create common chart

---
+++ argo.sh
@@ -0,0 +1,8 @@
+argocd login argocd.rdevopsb73.online --username admin --password $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d) --insecure --grpc-web
+
+for i in cart catalogue user payment shipping frontend ; do
+
+  argocd app create $i --repo https://github.com/raghudevopsb77/roboshop-helm-and-argo-jobs.git --path helm --dest-namespace default --dest-server https://kubernetes.default.svc --grpc-web --values dev/$i.yaml
+  argocd app sync $i
+
+done
